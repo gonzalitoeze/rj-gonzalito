f1241b42532fad502846ba4e2d3cfc7cf19817d8
Gonzalo <gonzabarreiro7@gmail.com>
2022-08-17 23:53:38 -0300

    Edit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
-import { Header } from './components/Header/Header';
 import { NavBar } from './components/NavBar/NavBar';
+import { Header } from './components/Header/Header';
 import { Button } from 'react-bootstrap';
 
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,11 +6,13 @@ import './NavBar.scss';
 
 export const NavBar = () => {
     return (
-        <div className="Nav">
-            <li><a href='men.js'>Men's</a></li>
-            <li><a href='wemen.js'>Women's</a></li>
-            <li><a href='accesories.js'>Accesories</a></li>
-            <li><a href='contact.js'>Contact</a></li>
+        <div className='NavBar'>
+            <div className="Nav">
+                <li><a href='men.js'>Men's</a></li>
+                <li><a href='wemen.js'>Women's</a></li>
+                <li><a href='accesories.js'>Accesories</a></li>
+                <li><a href='contact.js'>Contact</a></li>
+            </div>
         </div>
     )
 }
